93d8b3a83e149e158d25645889859e05e1c0fee0
rmsepulveda <rmsepulveda@sbcglobal.net>
2021-09-16 12:45:46 -0700

    Update urlChecker.js

--- src/client/js/urlChecker.js
+++ src/client/js/urlChecker.js
@@ -1,10 +1,6 @@
 function checkForURL(inputURL) {
-    let regex = inputURL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-     if(regex == null){
-         return 0;
-     } else{
-     return 1;
-     }
+    let chkURL = inputURL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return (chkURL !== null)
  }
 
  export { checkForURL }
