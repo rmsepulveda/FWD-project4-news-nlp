40ae9e285a9d4c3bc2b7a08b5e4c40ac6bd470b3
rmsepulveda <rmsepulveda@sbcglobal.net>
2021-09-16 12:47:20 -0700

    CHANGES

--- src/client/js/urlTester.js
+++ src/client/js/urlTester.js
@@ -1,4 +1,4 @@
-function checkForURL(inputURL) {
+function checkIfURL(inputURL) {
     let chkURL = inputURL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
     return (chkURL !== null)
  }
